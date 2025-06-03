97911296361492e496c6fea15c6f590662aca1c8
CosmosViajes <cosmosviajes23@gmail.com>
2025-06-03 14:10:56 +0200

    prueba c10

--- src/app/components/flights-list/flights-list.component.ts
+++ src/app/components/flights-list/flights-list.component.ts
@@ -83,15 +83,13 @@ import { switchMap, takeWhile, distinctUntilChanged } from 'rxjs/operators';
           </button>
         </div>
       } @else {
-        @if (filteredFlights.length > 0) {
           @for (flight of filteredFlights; track flight.id) {
             <app-flight-card
               [flight]="flight"
               class="flight-card-grid"
             ></app-flight-card>
-          }
-        } @else {
-          <div class="col-span-full flex flex-col items-center py-12">
+          } @empty{
+           <div class="col-span-full flex flex-col items-center py-12">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 mx-auto text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
             </svg>
@@ -99,7 +97,7 @@ import { switchMap, takeWhile, distinctUntilChanged } from 'rxjs/operators';
               No hay vuelos disponibles
             </h3>
           </div>
-        }
+          }
       }
     }
   </div>
